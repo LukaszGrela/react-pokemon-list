f73680dc4f71c6f9526dd97f568b48b5c5d721df
Lukasz <lukasz.grela@gmail.com>
2018-03-18 22:51:32 +0000

    pokemonReducer completed with test suite.

--- src/reducers/pokemonReducer.js
+++ src/reducers/pokemonReducer.js
@@ -1,8 +1,36 @@
+import { GET_POKEMON_LIST_STARTED, GET_POKEMON_LIST_FINISHED } from "../actions/actionPokemonList";
+
 export const DEFAULT_POKEMONS_STATE = {
+    count: 0,
     page: 1,
-    list: []
+    list: [],
+    error: null,
+    loading: false,
+    noMore: false
 }
 const pokemonReducer = (state = DEFAULT_POKEMONS_STATE, action) => {
+
+    switch (action.type) {
+        case GET_POKEMON_LIST_STARTED:
+            return { ...state, error: null, loading: true };
+            break;
+        case GET_POKEMON_LIST_FINISHED:
+            const success = action.success;
+            let newState = { ...state, loading: false, page: action.page };
+            if (success) {
+                newState.noMore = action.payload.next === null;
+                newState.list = [...state.list, ...action.payload.results];
+                newState.count = action.payload.count;
+            } else {
+                newState.error = action.payload;
+            }
+            return newState;
+            break;
+        default:
+            break;
+    }
+
+
     return state;
 };
 export default pokemonReducer;
---
+++ src/tests/reducers/pokemonReducer.test.js
@@ -0,0 +1,139 @@
+import pokemonReducer, { DEFAULT_POKEMONS_STATE } from "../../reducers/pokemonReducer";
+import { actionGetPokemonList, actionGetPokemonListStarted, actionGetPokemonListFinished } from "../../actions/actionPokemonList";
+import { POKEMON_LIST } from "../fixtures/pokemon-list";
+
+test('Should set default state', () => {
+    const state = pokemonReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual(DEFAULT_POKEMONS_STATE);
+});
+
+test('should change loading to true for GET_POKEMON_LIST_STARTED action', () => {
+    const state = pokemonReducer(undefined, actionGetPokemonListStarted());
+
+    const newState = {
+        ...DEFAULT_POKEMONS_STATE,
+        loading: true
+    };
+
+    expect(state).toEqual(newState);
+});
+
+
+test('should change loading to false for GET_POKEMON_LIST_FINISHED action', () => {
+    const prevState = {
+        ...DEFAULT_POKEMONS_STATE,
+        loading: true
+    }
+    const payload = {
+        ...POKEMON_LIST,
+        results: [],
+        count:0
+    };
+    const state = pokemonReducer(prevState, actionGetPokemonListFinished(1, payload, true));
+
+    const newState = {
+        ...DEFAULT_POKEMONS_STATE,
+        loading: false
+    };
+
+    expect(state).toEqual(newState);
+});
+
+
+test('should change page for GET_POKEMON_LIST_FINISHED action', () => {
+    const prevState = {
+        ...DEFAULT_POKEMONS_STATE
+    }
+    const payload = {
+        ...POKEMON_LIST,
+        results: [],
+        count:0
+    };
+    const state = pokemonReducer(prevState, actionGetPokemonListFinished(2, payload, true));
+
+    const newState = {
+        ...DEFAULT_POKEMONS_STATE,
+        page: 2
+    };
+
+    expect(state).toEqual(newState);
+});
+
+
+test('should change noMore to true for GET_POKEMON_LIST_FINISHED action', () => {
+    const prevState = {
+        ...DEFAULT_POKEMONS_STATE
+    };
+    const payload = {
+        ...POKEMON_LIST,
+        results: [],
+        count:0,
+        next: null
+    };
+    const state = pokemonReducer(prevState, actionGetPokemonListFinished(1, payload, true));
+
+    const newState = {
+        ...DEFAULT_POKEMONS_STATE,
+        noMore: true
+    };
+
+    expect(state).toEqual(newState);
+});
+
+test('should change count for GET_POKEMON_LIST_FINISHED action', () => {
+    const prevState = {
+        ...DEFAULT_POKEMONS_STATE
+    };
+    const payload = {
+        ...POKEMON_LIST
+    };
+    const state = pokemonReducer(prevState, actionGetPokemonListFinished(1, payload, true));
+
+    const newState = {
+        ...DEFAULT_POKEMONS_STATE,
+        list:POKEMON_LIST.results,
+        count: POKEMON_LIST.count
+    };
+
+    expect(state).toEqual(newState);
+});
+
+
+test('should change list for GET_POKEMON_LIST_FINISHED action', () => {
+    const prevState = {
+        ...DEFAULT_POKEMONS_STATE,
+        list:["one"]
+    };
+    const payload = {
+        ...POKEMON_LIST
+    };
+    const state = pokemonReducer(prevState, actionGetPokemonListFinished(1, payload, true));
+
+    const newState = {
+        ...DEFAULT_POKEMONS_STATE,
+        list:[...prevState.list, ...POKEMON_LIST.results],
+        count: POKEMON_LIST.count
+    };
+
+    expect(state).toEqual(newState);
+});
+
+
+test('should change error to be not null for GET_POKEMON_LIST_FINISHED failed action', () => {
+    const prevState = {
+        ...DEFAULT_POKEMONS_STATE
+    };
+    const payload = {
+        ...POKEMON_LIST,
+        next: null
+    };
+    const state = pokemonReducer(prevState, actionGetPokemonListFinished(1, { error: "error" }, false));
+
+    const newState = {
+        ...DEFAULT_POKEMONS_STATE,
+        error: { error: 'error' }
+    };
+
+    expect(state).toEqual(newState);
+});
+

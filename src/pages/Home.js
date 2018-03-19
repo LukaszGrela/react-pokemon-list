2f957693884025d33b3b3f73b931266de4838a72
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-03-19 22:14:40 +0000

    Spinner loading indicator.  Prepare Home for 'infinite' loading. Styles.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,23 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { actionGetPokemonList } from '../actions/actionPokemonList';
 import PokemonList from '../components/PokemonList';
+import Spinner from '../components/Spinner';
 
-
+import './styles/Home.scss';
 
 class Home extends React.Component {
     render = () => {
+        const {list, loading} = this.props;
         return (
             <article className='home'>
-                <PokemonList list={this.props.list} />
-                <button onClick={() => {
-                    this.props.pullMoreItems();
-                }}>Pull More Items</button>
+                <PokemonList list={list} />
+                {loading && <Spinner />}
             </article>
         );
     }
 };
 
 const mapStateToProps = (state, props) => ({
+    loading:state.pokemons.loading,
+    error: state.pokemons.error,
+    noMore: state.pokemons.noMore,
     list: state.pokemons.list
 });
 const mapDispatchTopProps = (dispatch) => ({

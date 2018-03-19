e1faedf15e0c3c9797f41e4fec3bd97bfbc621cd
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-03-19 20:47:01 +0000

    change  sprite call to use local images

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -7,11 +7,14 @@ import './styles/PokemonList.scss';
 export default class PokemonList extends React.Component {
     render = () => {
         return (
-            <ul className='list'>
+            <ul className='pokemon-list'>
             {
-                this.props.list.map(({ url, name, id }, index) => <li key={index}>
-                    <img src={API_GET_SPRITE_FRONT(id)} />
-                    <div className='name'>{name}</div>
+                this.props.list.map(({ url, name, id }, index) => <li key={index} className='pokemon-item'>
+                    <img src={API_GET_SPRITE_FRONT(id)} className='pokemon-image'
+                    onError={(e) => {
+                        e.target.src = API_GET_SPRITE_FRONT('default/0');
+                    }} />
+                    <div className='pokemon-name'>{name}</div>
                 </li>)
             }
         </ul>)

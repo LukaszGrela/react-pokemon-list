78e898114af31303ca3b4dafd8b938e1d55a654e
Lukasz <lukasz.grela@gmail.com>
2018-03-21 07:46:21 +0000

    PokemonDetails in progress...

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+import './styles/PokemonDetails.scss';
+import {
+    API_GET_SPRITE_FRONT,
+    API_GET_SPRITE_BACK
+} from '../constants/api';
 
 class PokemonDetails extends React.Component {
     render = () => {
+        const { id } = this.props;
         return (
-            <div>PokemonDetails</div>
+            <div className='pokemon-details'>
+                <div className='pokemon-image'>
+                    <img src={API_GET_SPRITE_FRONT(id)} className='front'
+                        onError={(e) => {
+                            e.target.src = API_GET_SPRITE_FRONT('default/0');
+                        }} />
+
+                </div>
+                <div className='pokemon-name'>Name of the Pokemon</div>
+                <div className='pokemon-stats'>Loading...</div>
+                <div className='pokemon-evolution'>Loading...</div>
+            </div>
         );
     }
 }
 
-export default PokemonDetails;
+PokemonDetails.propTypes = {
+    id: PropTypes.number.isRequired
+};
+
+const mapStateToProps = (state, props) => ({
+    /* selectors pokemon list, pokemon details */
+});
+const mapDispatchToProps = (dispatch) => ({
+    /* action to pull pokemon data  */
+});
+
+export default connect(mapStateToProps)(PokemonDetails);

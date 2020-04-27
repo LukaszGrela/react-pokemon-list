a7f33d59c6ef9a7eca2a86fd02d4ffefded929f0
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2020-04-28 00:50:17 +0100

    Home component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import PokemonList from '../PokemonList/PokemonList';
+import './style/index.scss';
+
+const Home: React.FC = (): JSX.Element => {
+  const { page } = useParams();
+
+  return (
+    <main className='Home'>
+      <header>
+        <h1>Pokémon List</h1>
+      </header>
+      <PokemonList page={page} />
+      <footer>{page}</footer>
+    </main>
+  );
+};
+
+export default Home;

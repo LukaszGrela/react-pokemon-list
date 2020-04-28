91e0def8f53986e6cedd1bc73ba5776350f9c834
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2020-04-28 01:22:39 +0100

    render list items

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { getPokemonList } from '../../api';
+import React, { useEffect, useState, ReactNode } from 'react';
+import { getPokemonList, parseIdFromUrl } from '../../api';
+import { INamedAPIResource, IAPIResourceList } from '../../store/model/pokemon';
+import PokemonListItem from './PokemonListItem';
 
 export interface IProps {
   page: number;
@@ -8,14 +10,16 @@ export interface IProps {
 const PokemonList: React.FC<IProps> = (props: IProps): JSX.Element => {
   const { page } = props;
   const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState<INamedAPIResource[]>([]);
 
   useEffect((): (() => void) => {
     setLoading(true);
     const { signal, abort } = new AbortController();
     fetch(getPokemonList(page), { signal })
       .then((r) => r.json())
-      .then((data) => {
+      .then((data: IAPIResourceList) => {
         console.log(data);
+        setResults(data.results);
       })
       .catch((e: Error) => {
         if (e.name !== 'AbortError') {
@@ -30,7 +34,27 @@ const PokemonList: React.FC<IProps> = (props: IProps): JSX.Element => {
     };
   }, [page]);
 
-  return <>{loading && <ul className='PokemonList'></ul>}</>;
+  return (
+    <>
+      {loading && <div className='PokemonList'>Loading...</div>}
+      {!loading && (
+        <ul className='PokemonList'>
+          {results.map(
+            (resource: INamedAPIResource, i): ReactNode => {
+              return (
+                <PokemonListItem
+                  key={i + (page - 1) * 20 + 1}
+                  pid={`${parseIdFromUrl(resource.url) || ''}`}
+                  name={resource.name}
+                  n={i + (page - 1) * 20 + 1}
+                />
+              );
+            }
+          )}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default PokemonList;

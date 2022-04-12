5a2c202965522392cb328127167570e3ab01e46f
Lukasz <lukasz.grela@gmail.com>
2022-04-12 20:29:00 +0200

    Functional component escape to close, disable scroll on documentElement

--- src/components/Modal/types.ts
+++ src/components/Modal/types.ts
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
+import { ECloseModalEnum } from "./enums";
 
 export interface IProps {
   modalId?: string;
   children: ReactNode;
   isOpen?: boolean;
-  closeModal: () => void;
+  closeModal: (reason: ECloseModalEnum) => void;
   className?: string;
   overlayClassName?: string;
   disableOverlay?: boolean;

617e62061c38860d5565a99ed998eb10f00614b8
Lukasz <lukasz.grela@gmail.com>
2022-04-12 20:22:48 +0200

    rename file

--- src/components/Modal/consts.ts
+++
@@ -1,5 +0,0 @@
-export const MODAL_AUTO_DISMISSED = -2;
-export const MODAL_DISMISSED = -1;
-export const MODAL_ACTION_CANCEL = 0;
-export const MODAL_ACTION_CONFIRM = 1;
-export const MODAL_ACTION_NEUTRAL = 2;

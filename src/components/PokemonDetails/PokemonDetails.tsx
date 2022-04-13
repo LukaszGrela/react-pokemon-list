5f4dc00357def9402e0a4d209d911e02eaa64841
Lukasz <lukasz.grela@gmail.com>
2022-04-13 10:41:19 +0200

    query pokemon details It's cached! :)

--- src/components/PokemonDetails/PokemonDetails.tsx
+++ src/components/PokemonDetails/PokemonDetails.tsx
@@ -1,73 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   API_GET_SPRITE_FRONT,
   API_GET_SPRITE_BACK,
-  getPokemonDetail,
 } from '../../api';
-import { IPokemonDetail } from '../../store/model/pokemon';
+import { useGetPokemonByNameOrIdQuery } from '../../store/services/pokemon-details';
 import { Image } from '../Image';
+import Spinner from '../Spinner/Spinner';
 
 export interface IProps {
   pid: string;
 }
 
 const PokemonDetails: React.FC<IProps> = ({ pid }: IProps): JSX.Element => {
-  const [loading, setLoading] = useState(false);
-  const [results, setResults] = useState<IPokemonDetail | undefined>(undefined);
 
-  useEffect(() => {
-    const abortController = new AbortController();
-    setLoading(true);
+  // Using a query hook automatically fetches data and returns query values
+  const { data: results, error, isLoading: loading } = useGetPokemonByNameOrIdQuery(pid);
+  // Individual hooks are also accessible under the generated endpoints:
+  // const { data, error, isLoading } = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
 
-    fetch(getPokemonDetail(pid), { signal: abortController.signal })
-      .then((response) => response.json() as Promise<IPokemonDetail>)
-      .then((data) => setResults(data))
-      .catch((e: Error) => {
-        if (e.name !== 'AbortError') {
-          console.error(e);
-        }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-    return () => {
-      abortController.abort();
-    };
-  }, [pid]);
 
   return (
     <div className='PokemonDetails'>
       <div className='PokemonDetails_wrapper'>
-        <div className='pokemon-image column-left'>
-          <Image
-            src={API_GET_SPRITE_FRONT(pid)}
-            fallbackSrc={API_GET_SPRITE_FRONT('default/0')}
-            className='front'
-            alt={`Image of {name} pokemon.`}
-          />
-          <Image
-            src={API_GET_SPRITE_BACK(pid)}
-            fallbackSrc={API_GET_SPRITE_FRONT('default/0')}
-            className='back'
-            alt={`Image of the back of the {name} pokemon.`}
-          />
-        </div>
-        <div className='column-right'>
-          {results && (
-            <>
-              <div className='pokemon-name'>{results.name}</div>
-              <div className='pokemon-weight'>
-                <span className='label'>Weight: </span>
-                <span className='value'>{results.weight / 10 + 'kg'}</span>
-              </div>
-              <div className='pokemon-height'>
-                <span className='label'>Height: </span>
-                <span className='value'>{results.height / 10 + 'm'}</span>
-              </div>
-              {/* <PokemonStats stats={stats} /> */}
-            </>
-          )}
-        </div>
+        {loading && <Spinner />}
+        {!loading && <>
+          <div className='pokemon-image column-left'>
+            <Image
+              src={API_GET_SPRITE_FRONT(pid)}
+              fallbackSrc={API_GET_SPRITE_FRONT('default/0')}
+              className='front'
+              alt={`Image of {name} pokemon.`}
+            />
+            <Image
+              src={API_GET_SPRITE_BACK(pid)}
+              fallbackSrc={API_GET_SPRITE_FRONT('default/0')}
+              className='back'
+              alt={`Image of the back of the {name} pokemon.`}
+            />
+          </div>
+          <div className='column-right'>
+            {results && (
+              <>
+                <div className='pokemon-name'>{results.name}</div>
+                <div className='pokemon-weight'>
+                  <span className='label'>Weight: </span>
+                  <span className='value'>{results.weight / 10 + 'kg'}</span>
+                </div>
+                <div className='pokemon-height'>
+                  <span className='label'>Height: </span>
+                  <span className='value'>{results.height / 10 + 'm'}</span>
+                </div>
+                {/* <PokemonStats stats={stats} /> */}
+              </>
+            )}
+          </div>
+        </>}
       </div>
     </div>
   );
--- src/components/PokemonDetailsModalContent/PokemonDetailsModalContent.tsx
+++ src/components/PokemonDetailsModalContent/PokemonDetailsModalContent.tsx
@@ -1,4 +1,5 @@
-import PokemonDetails from '../PokemonDetails/PokemonDetails';
+import { useGetPokemonByNameOrIdQuery } from '../../store/services/pokemon-details';
+import { capitalise } from '../../utils/capitalise';
 import Spinner from '../Spinner/Spinner';
 import { IProps } from './types';
 
@@ -8,6 +9,10 @@ const PokemonDetailsModalContent: React.FC<IProps> = ({
   name,
   closeModal,
 }): JSX.Element => {
+  const { data, error, isLoading, } = useGetPokemonByNameOrIdQuery(pid);
+
+  const capitaliseName = capitalise(name);
+
   return (
     <div className='PokemonDetailsModalContent'>
       <header className='PokemonDetailsModalContent_header'>
@@ -15,14 +20,26 @@ const PokemonDetailsModalContent: React.FC<IProps> = ({
           id={`modal-${modalId}-title`}
           className='PokemonDetailsModalContent_title'
         >
-          {`Details of ${name}`}
+          {isLoading ? `Loading of ${capitaliseName}` : `Details of ${capitaliseName}`}
         </h2>
       </header>
       <section className='PokemonDetailsModalContent_body'>
-        <PokemonDetails pid={pid} />
+        {isLoading && <Spinner />}
+        {!isLoading && error && <p>{`${error}`}</p>}
+        {!isLoading && !error && data && <>
+          <div className='pokemon-weight'>
+            <span className='label'>Weight: </span>
+            <span className='value'>{data.weight / 10 + 'kg'}</span>
+          </div>
+          <div className='pokemon-height'>
+            <span className='label'>Height: </span>
+            <span className='value'>{data.height / 10 + 'm'}</span>
+          </div>
+        </>}
+        {!isLoading && !error && !data && <span>No data found</span>}
       </section>
       <footer className='PokemonDetailsModalContent_footer'>
-        <Spinner />
+
       </footer>
     </div>
   );

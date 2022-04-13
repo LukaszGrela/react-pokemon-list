d933e92ae195938f5bdb698ba75ef9e13bb7c9db
Lukasz <lukasz.grela@gmail.com>
2022-04-13 10:39:47 +0200

    capitalise pokemon name utility

---
+++ src/utils/capitalise.ts
@@ -0,0 +1,11 @@
+export const capitalise = (input: string): string => {
+  if (input.length === 0) return input;
+
+  const first = input.substring(0, 1).toLocaleUpperCase();
+  if (input.length === 1) return first;
+  if (input.indexOf('-') !== -1) {
+    return input.split('-').map(chunk => capitalise(chunk)).join('-')
+  } else {
+    return `${first}${input.substring(1)}`
+  }
+}

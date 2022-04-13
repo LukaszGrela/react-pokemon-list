7721f4944dd356d05ffa91e06823af4379460cc8
Lukasz <lukasz.grela@gmail.com>
2022-04-13 23:51:23 +0200

    set pokemons list reducer

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -2,6 +2,7 @@ import { createBrowserHistory } from 'history';
 import { combineReducers, Reducer, AnyAction } from 'redux';
 import { createReduxHistoryContext, RouterActions, RouterState } from "redux-first-history";
 import { pokemonDetails, TPokemonDetailsReducer } from './services/pokemon-details';
+import { pokemonsList, TPokemonsListReducer } from './services/pokemons-list';
 
 
 export const { createReduxHistory, routerMiddleware, routerReducer } = createReduxHistoryContext({
@@ -15,6 +16,7 @@ export type TRootReducer = Reducer<
   {
     router: RouterState;
     [pokemonDetails.reducerPath]: TPokemonDetailsReducer;
+    [pokemonsList.reducerPath]: TPokemonsListReducer;
   },
   TAction
 >;
@@ -23,6 +25,7 @@ const createRootReducer = (): TRootReducer =>
   combineReducers({
     router: routerReducer,
     // Add the generated reducer as a specific top-level slice
+    [pokemonsList.reducerPath]: pokemonsList.reducer,
     [pokemonDetails.reducerPath]: pokemonDetails.reducer,
   });
 

85f8f9ed98a4a42a868bbc4da5b274a7bb8bf594
Lukasz <lukasz.grela@gmail.com>
2022-04-15 00:18:15 +0200

    WIP: Silhouette image using canvas

--- src/components/SilhouetteImage/SilhouetteImage.tsx
+++ src/components/SilhouetteImage/SilhouetteImage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef } from 'react';
-import { Image } from '../Image';
+import { rgbToChannels, TRGB } from '../../utils/colors';
 
 import './style/index.scss';
 
@@ -8,25 +8,10 @@ export interface IProps {
   src: TPathToPNG;
 }
 
-function getBase64FromImageUrl(url: string): HTMLCanvasElement {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-  const img = new global.Image();
-  img.setAttribute('crossOrigin', 'anonymous');
-  img.onload = function () {
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    ctx?.drawImage(img, 0, 0);
-  };
-  img.onerror = console.error;
-
-  return canvas;
-}
-
 const useCanvas = (
   url: TPathToPNG,
-  trim: boolean = false
+  trim: boolean = false,
+  color: TRGB = 0
 ): React.RefObject<HTMLCanvasElement> => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const img = useMemo(() => new global.Image(), []);
@@ -42,12 +27,12 @@ const useCanvas = (
       img.onload = function () {
         canvas.width = img.width;
         canvas.height = img.height;
-
         context?.drawImage(img, 0, 0);
+
         if (trim) {
           // bottom-trim
-          let trimTo = -1;
-          bottomTrim: for (let r = img.height; --r >= 0; ) {
+          let trimBottom = -1;
+          bottomTrim: for (let r = img.height; --r >= 0;) {
             // row by row
             for (let c = 0; c < img.width; c++) {
               // columns
@@ -55,46 +40,76 @@ const useCanvas = (
                 colorSpace: 'srgb',
               });
               if (data && data.data && data.data[3] === 255) {
-                trimTo = r + 1;
-                console.log('break', r, c, data);
+                trimBottom = r + 1;
+                console.log(data.data)
                 break bottomTrim;
               }
             }
           }
           let trimTop = -1;
-          console.log('trimTo', trimTo, canvas.height);
-          if (trimTo > 0) {
-            canvas.height = trimTo;
-            context?.clearRect(0,0,img.width, trimTo);
-            context?.drawImage(img, 0, 0, img.width, trimTo, 0, 0, img.width, trimTo);
+          topTrim: for (let r = 0; r < (trimBottom !== -1 ? trimBottom : img.height); r++) {
+            // top-trim
+            for (let c = 0; c < img.width; c++) {
+              // columns
+              const data = context?.getImageData(c, r, 1, 1, {
+                colorSpace: 'srgb',
+              });
+              if (data && data.data && data.data[3] === 255) {
+                trimTop = r - 1;
+                break topTrim;
+              }
+            }
           }
+
+          if (trimBottom > 0 || trimTop > 0) {
+
+            const newHeight = (trimBottom !== -1 ? trimBottom : img.height) - Math.max(trimTop, 0);
+            canvas.height = newHeight;
+
+            context?.clearRect(0, 0, img.width, img.height);
+            context?.drawImage(img, 0, Math.max(trimTop, 0), img.width, newHeight, 0, 0, img.width, newHeight);
+          }
+        }
+        // paint it
+        if (context) {
+          const { width, height } = canvas;
+          const imageData = context.getImageData(0, 0, width, height);
+          const fillData = rgbToChannels(color);
+          const r = 0, g = 1, b = 2, a = 3;
+          for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+              const pos = y * width + x;
+              const alpha = imageData.data[pos * 4 + 3]
+              if (alpha > 0) {
+                // r
+                imageData.data[pos * 4 + r] = fillData[r];
+                // g
+                imageData.data[pos * 4 + g] = fillData[g];
+                // b
+                imageData.data[pos * 4 + b] = fillData[b];
+              }
+            }
+          }
+          // set
+          context.putImageData(imageData, 0, 0)
         }
       };
       img.onerror = console.error;
     }
-    return () => {};
-  }, [img, url, trim]);
+    return () => { };
+  }, [img, url, trim, color]);
 
   return canvasRef;
 };
 
 const SilhouetteImage: React.FC<IProps> = ({ src }: IProps): JSX.Element => {
-  const canvasRef = useCanvas(src);
   const canvas2Ref = useCanvas(src, true);
 
   return (
     <div
       className='SilhouetteImage'
-      style={{
-        WebkitMaskImage: `url(${src})`,
-        maskImage: `url(${src})`,
-      }}
     >
-      <>
-        <canvas ref={canvasRef} />
-        <canvas ref={canvas2Ref} />
-        <Image src={src} className='SilhouetteImage_silhouette' />
-      </>
+      <canvas ref={canvas2Ref} className='SilhouetteImage_silhouette' />
     </div>
   );
 };

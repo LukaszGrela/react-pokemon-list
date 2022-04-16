e6d4f7eedf3af03f9d78d9fa6857ed612285915d
Lukasz <lukasz.grela@gmail.com>
2022-04-16 21:56:02 +0200

    handle the li'l ones

--- src/components/HeightCompare/HeightCompare.tsx
+++ src/components/HeightCompare/HeightCompare.tsx
@@ -21,15 +21,21 @@ export interface IProps {
 const HeightCompare: React.FC<IProps> = ({ src, baseHeight, height }): JSX.Element => {
   const heightMax = Math.max(baseHeight, height);
   const heightMin = Math.min(baseHeight, height);
-  const scale = (heightMin / heightMax) * 100;
+  let scale = (heightMin / heightMax) * 100;
+  let viewBox: string | undefined;
+
+  if (scale < 17) {
+    scale = 4 * Math.round(scale);
+    viewBox = '0 375 165.175 125'
+  }
 
   return (
     <div className='HeightCompare'>
       <div style={{ height: baseHeight > height ? '100%' : `${scale}%` }}>
-        <StandingMan />
+        <StandingMan viewBox={viewBox} />
       </div>
       <div style={{ height: baseHeight < height ? '100%' : `${scale}%` }}>
-        <SilhouetteImage src={src} />
+        <SilhouetteImage src={src} color={0} />
       </div>
     </div>
   );

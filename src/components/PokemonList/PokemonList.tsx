ff8d3ccad231e4f1b071e217f8c8d09d68e8a852
Lukasz <lukasz.grela@gmail.com>
2022-04-17 11:50:28 +0200

    set loading spinner

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -3,6 +3,7 @@ import { PAGINATION, parseIdFromUrl } from '../../api';
 import type { TNamedAPIResource } from '../../store/model/common';
 import { useGetPokemonsListQuery } from '../../store/services/pokemons-list';
 import PokemonListItem from '../PokemonListItem/PokemonListItem';
+import Spinner from '../Spinner/Spinner';
 
 import './style/index.scss';
 export interface IProps {
@@ -18,26 +19,27 @@ const PokemonList: React.FC<IProps> = (props: IProps): JSX.Element => {
   const { data: results, error, isLoading: loading, } = useGetPokemonsListQuery(PAGINATION(page));
 
   return (
-    <>
-      {loading && <div className='PokemonList PokemonList_loading'>Loading...</div>}
-      {!loading && (
-        <ul className={`PokemonList${interactive ? ' interactive' : ''}`}>
-          {results?.results.map(
-            (resource: TNamedAPIResource): ReactNode => {
-              console.log(resource)
-              const key = resource.url;
-              return (
-                <PokemonListItem
-                  key={key}
-                  pid={`${parseIdFromUrl(resource.url) || ''}`}
-                  name={resource.name}
-                  onClick={(pid) => handlePokemonSelect(pid, resource.name)}
-                />
-              );
-            }
-          )}
-        </ul>
-      )}
+    <>{!loading && (
+      <ul className={`PokemonList${interactive ? ' interactive' : ''}`}>
+        {results?.results.map(
+          (resource: TNamedAPIResource): ReactNode => {
+            console.log(resource)
+            const key = resource.url;
+            return (
+              <PokemonListItem
+                key={key}
+                pid={`${parseIdFromUrl(resource.url) || ''}`}
+                name={resource.name}
+                onClick={(pid) => handlePokemonSelect(pid, resource.name)}
+              />
+            );
+          }
+        )}
+      </ul>
+    )}
+      {!loading && !error && !results && <span>No data found</span>}
+      {loading && <div className='PokemonList PokemonList_loading'><Spinner /></div>}
+      {!loading && error && <p>{`${error}`}</p>}
     </>
   );
 };

4e073362d00efbfe9f83b8f0fab1b2f6edd12731
Lukasz <lukasz.grela@gmail.com>
2022-04-18 19:30:38 +0200

    loading state in Image component

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Spinner from '../Spinner/Spinner';
 import { IProps } from './types';
+import './style/index.scss';
 
 const Image: React.FC<IProps> = ({
   src,
   alt,
   className,
   fallbackSrc,
+  onError,
+  onLoaded,
 }): JSX.Element => {
+  const [loading, setLoading] = React.useState(false);
   const [hasErrors, setHasErrors] = React.useState(false);
+  useEffect(() => {
+    setLoading(true);
+  }, [src]);
+
   const handleError = React.useCallback<
     React.ReactEventHandler<HTMLImageElement>
   >(
     (e) => {
+      if (!hasErrors) {
+        onError?.({ message: 'Failed to load image' });
+      } else {
+        onError?.({ message: 'Failed to load fallback image' });
+      }
       if (fallbackSrc && !hasErrors) {
         setHasErrors(true);
         e.currentTarget.src = fallbackSrc;
       }
     },
-    [fallbackSrc, hasErrors]
+    [fallbackSrc, hasErrors, onError]
   );
+
   return (
-    <img
-      src={src}
-      alt={alt}
-      className={`Image${className ? ` ${className}` : ''}`}
-      onError={handleError}
-      aria-hidden={!alt}
-    />
+    <span className={`Image${className ? ` ${className}` : ''}`}>
+      <img
+        className={`Image_img${loading ? ' loading' : ''}`}
+        src={src}
+        alt={alt}
+        onError={handleError}
+        aria-hidden={!alt}
+        onLoad={() => {
+          onLoaded?.(hasErrors);
+          setLoading(false);
+        }}
+      />
+      {loading && <Spinner />}
+    </span>
   );
 };
 
--- src/components/Image/types.ts
+++ src/components/Image/types.ts
@@ -3,4 +3,7 @@ export interface IProps {
   fallbackSrc?: string;
   alt?: string;
   className?: string;
+
+  onError?: (error: { message: string }) => void;
+  onLoaded?: (fallback?: boolean) => void;
 }
